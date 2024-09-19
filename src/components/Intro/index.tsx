bdc1957c8b8add2c85dfddd761b8351592cae543
Caleb Gomes Pinto <caleb.gp.dev@gmail.com>
2024-09-19 22:36:02 +0100

    doing: connections..

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -5,14 +5,15 @@ import styles from "./Intro.module.css";
 const Intro: React.FC<{ basics: Basics | undefined; }> = ({ basics }) => {
     return (
         <div className={styles.intro}>
+            <img className={styles.myImage} src={basics?.image}/>
             <div className={styles.introText}>
-                <h1>{basics?.name || <Skeleton />}</h1>
-                <p>{basics?.label || <Skeleton />}</p>
-                <p>{basics?.headline || <Skeleton />}</p>
-                <p>{basics?.summary || <Skeleton count={2} />}</p>
+                <h1>{basics?.name || <Skeleton/>}</h1>
+                <p>{basics?.label || <Skeleton/>}</p>
+                <p>{basics?.headline || <Skeleton/>}</p>
+                <p>{basics?.summary || <Skeleton count={2}/>}</p>
 
             </div>
-            <img className={styles.myImage} src={basics?.image} />
+
         </div>
     );
 };

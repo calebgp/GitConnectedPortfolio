8e4ef40bf1b649407181348344a04674cc4b2811
calebpinto.30519 <calebpinto.30519@ae-smfeira.pt>
2024-10-11 16:33:51 +0000

    feat: projects's grid

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -2,15 +2,21 @@ import React, { useContext } from 'react';
 import styles from './Projects.module.css';
 import ProjectW from '../../components/Project';
 import { ProfileContext } from '../../main';
+import Grid2 from '@mui/material/Grid2';
 
 const Projects: React.FC = () => {
   const profile = useContext(ProfileContext);
   return (
     <div className={styles.container}>
+      <Grid2 container spacing={4}>
         {profile?.projects.map((project, index) => (
-          <ProjectW key={index} project={project} />
+          <Grid2 item xs={4} key={index}>
+            <ProjectW project={project} />
+          </Grid2>
         ))}
-    </div >
+      </Grid2>
+    </div>
+
   );
 };
 

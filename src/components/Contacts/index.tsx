c9cd76e53b4ef40556bf1a0f28810e54a758218b
Caleb Gomes Pinto <caleb.gp.dev@gmail.com>
2025-01-02 01:51:16 +0000

    doing; contact form

--- src/components/Contacts/index.tsx
+++ src/components/Contacts/index.tsx
@@ -1,19 +1,25 @@
 import Skeleton from "react-loading-skeleton";
-import { Basics, } from "../../models/gc-profile";
+import { Basics } from "../../models/gc-profile";
 import styles from "./Contacts.module.css";
 import React from "react";
 
-const Contacts: React.FC<{ basics: Basics | undefined; }> = ({ basics }) => {
-
-    return (
-        <div className={styles.contacts}>
-            <div className={styles.contactsText}>
-                <p >Email: <a href={`mailto:${basics?.email}`}>{basics?.email|| <Skeleton/>}</a></p>
-            </div>
-
-        </div>
-    );
+const Contacts: React.FC<{ basics: Basics | undefined }> = ({ basics }) => {
+  return (
+    <div className={styles.contacts}>
+      <div className={styles.contactsText}>
+        <p>
+          Email:{" "}
+          <a href={`mailto:${basics?.email}`}>
+            {basics?.email || <Skeleton />}
+          </a>
+        </p>
+        <p>
+          Phone:{" "}
+          <a href={`tel:${basics?.phone}`}>{basics?.phone || <Skeleton />}</a>
+        </p>
+      </div>
+    </div>
+  );
 };
-
 
 export default Contacts;
--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -1,19 +1,18 @@
-import React, {useContext} from 'react';
-import styles from './ContactMe.module.css';
-import {Box, Divider} from "@mui/material";
-import {ProfileContext} from "../../main.tsx";
+import React, { useContext } from "react";
+import styles from "./ContactMe.module.css";
+import { Box, Divider } from "@mui/material";
+import { ProfileContext } from "../../main.tsx";
 import Contacts from "../../components/Contacts";
 
 const ContactMe: React.FC = () => {
-
-    const profile = useContext(ProfileContext);
-    return (
-        <Box className={styles.container}>
-            <Contacts basics={profile?.basics} />
-            <Divider flexItem color={"#999999"} sx={{opacity: 0.6}}/>
-        </Box>
-
-    );
+  const profile = useContext(ProfileContext);
+  return (
+    <Box className={styles.container}>
+      <Contacts basics={profile?.basics} />
+      <Divider flexItem color={"#999999"} sx={{ opacity: 0.6 }} />
+      {/* <ContactForm /> */}
+    </Box>
+  );
 };
 
 export default ContactMe;
